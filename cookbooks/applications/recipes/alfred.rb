7cab580afe3f0b36d4f9578a502ea2424f5464ba
Ken Robertson <ken@invalidlogic.com>
2011-07-04 16:03:37 -0700

    Fix volume name for alfred

--- cookbooks/applications/recipes/alfred.rb
+++ cookbooks/applications/recipes/alfred.rb
@@ -1,4 +1,5 @@
 dmg_package "Alfred" do
+  volumes_dir "Alfred.app"
   source "http://media.alfredapp.com/alfred_0.9.1_129.dmg"
   checksum "16e4eb5df6ece46a691a20e3e9c457e5c33af073c3a2ecaa3ba55a4923fba464"
 end
