0e5b9d853391da89eb066068a95b3b8f483ac738
Ken Robertson <ken@invalidlogic.com>
2011-07-04 15:59:23 -0700

    Dont autohide dock

--- cookbooks/osx/recipes/dock_preferences.rb
+++ cookbooks/osx/recipes/dock_preferences.rb
@@ -2,10 +2,6 @@ execute "set dock to be on right" do
   command "defaults write com.apple.dock orientation -string right"
 end
 
-execute "set dock to autohide" do
-  command "defaults write com.apple.dock autohide -boolean true"
-end
-
 execute "relaunch dock" do
   command "sudo killall Dock"
 end
