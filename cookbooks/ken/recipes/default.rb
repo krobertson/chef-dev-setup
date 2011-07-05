282bc6028ca5c3b963e8a2409030cf7855fcd6b2
Ken Robertson <ken@invalidlogic.com>
2011-07-05 09:12:16 -0700

    Moved configs to dotfiles

--- cookbooks/ken/recipes/default.rb
+++ cookbooks/ken/recipes/default.rb
@@ -2,14 +2,14 @@ execute "set user shell" do
   command "sudo chsh -s /bin/zsh #{node[:current_user]}"
 end
 
-git "/Users/#{node[:current_user]}/.configs" do
-  repository 'https://github.com/krobertson/configs.git'
+git "/Users/#{node[:current_user]}/.dotfiles" do
+  repository 'https://github.com/krobertson/dotfiles.git'
   reference "master"
   action :sync
   notifies :run, "execute[update configs symlinks]"
 end
 
 execute "update configs symlinks" do
-  cwd "/Users/#{node[:current_user]}/.configs"
+  cwd "/Users/#{node[:current_user]}/.dotfiles"
   command "rake symlink:force"
 end
