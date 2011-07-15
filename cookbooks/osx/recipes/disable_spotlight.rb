7a8c1babd1c9fd7293f26f8d37db7cfb9173ad66
Ken Robertson <ken@invalidlogic.com>
2011-07-15 07:01:51 -0700

    Removed disabling of spotlight, since it affected Alfred and Mail searching

--- cookbooks/osx/recipes/disable_spotlight.rb
+++
@@ -1,9 +0,0 @@
-# This is to completely disable the 'mds' process for Spotlight
-# on Snow Leopard
-#
-# The command comes from http://osxdaily.com/2009/09/20/disable-spotlight-in-mac-os-x-10-6-snow-leopard/
-
-execute "disable spotlight" do
-  command "sudo mdutil -a -i off"
-  only_if { %x{sudo mdutil -a -s} !~ /Indexing disabled/ }
-end
