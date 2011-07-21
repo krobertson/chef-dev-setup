ff1ea64fec1cdbe982cb949fd19c0ebb886f6c39
Ken Robertson <ken@invalidlogic.com>
2011-07-20 18:18:32 -0700

    More preferences

--- cookbooks/osx/recipes/dock_preferences.rb
+++ cookbooks/osx/recipes/dock_preferences.rb
@@ -2,6 +2,13 @@ execute "set dock to be on right" do
   command "defaults write com.apple.dock orientation -string right"
 end
 
+execute "set top right corner to put the display to sleep" do
+  command %Q{
+    defaults write com.apple.dock wvous-tr-corner 10
+    defaults write com.apple.dock wvous-tr-modifier 0
+  }
+end
+
 execute "relaunch dock" do
   command "sudo killall Dock"
 end
---
+++ cookbooks/osx/recipes/multitouch.rb
@@ -0,0 +1,3 @@
+execute "turn on right click with trackpad" do
+  command "defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick 1"
+end
